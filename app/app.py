9aca214bb4e28bd0d6c4a3147b2cd85bfedbf981
chr richter <christian_richter@gmx.de>
2023-09-11 23:12:47 +0200

    run email validation

--- app/app.py
+++ app/app.py
@@ -1,8 +1,15 @@
-import waitress
-from flask import Flask, Response, request, app, make_response
 import os
+from ipaddress import IPv4Address
+
+import waitress
+from flask import Flask
+from flask import Response
+from flask import jsonify
+from flask import make_response
+from validate_email import validate_email
 
 app = Flask(__name__)
+
 
 @app.route("/square/<int:base>", methods=["GET"])
 def square(base): # pylint: disable=unused-variable
@@ -13,9 +20,38 @@ def square(base): # pylint: disable=unused-variable
 
     return resp
 
+
 @app.route("/health", methods=["GET"])
-def health(): 
+def health():
     return make_response("", 200)
+
+
+@app.route("/email/validate/<string:email>", methods=["GET"])
+def verify_email(email):
+
+    hostname = os.getenv('HOSTNAME')
+    from_address = os.getenv('FROM_ADDRESS')
+
+    print(f'running on {hostname} and using email sender address {from_address}')
+
+    print(f'checking {email}')
+
+    is_valid = validate_email(
+        email_address=email,
+        check_format=True,
+        check_blacklist=True,
+        check_dns=True,
+        dns_timeout=10,
+        check_smtp=True,
+        smtp_timeout=10,
+        smtp_helo_host=hostname,
+        smtp_from_address=from_address,
+        smtp_skip_tls=False,
+        smtp_tls_context=None,
+        smtp_debug=True,
+        address_types=frozenset([IPv4Address]))
+
+    return jsonify(is_valid)
 
 
 waitress.serve(app, host="0.0.0.0", port=int(os.getenv("SERVICE_PORT")))
